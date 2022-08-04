91bddd43d219ce071cba3b0644c34ca00927b74d
EDR3X <dhunganaannanta@gmail.com>
2022-08-05 00:02:49 +0545

    modules

---
+++ 5.module_system/use_keyword/src/lib.rs
@@ -0,0 +1,18 @@
+mod front_of_house;
+
+//? not ideal approach */
+pub fn eat_at_restaurant() {
+    front_of_house::hosting::add_to_waitlist();
+    front_of_house::hosting::add_to_waitlist();
+    front_of_house::hosting::add_to_waitlist();
+}
+
+//? using use keyword
+
+// use crate::front_of_house::hosting; //* absolute path */
+use front_of_house::hosting; //* Relative path */
+pub fn eat_at_restaurant1() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
